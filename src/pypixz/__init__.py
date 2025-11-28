dfe850dafc75c13f7aad574488122f30a697a7d0
KDUser <128939316+KDUser12@users.noreply.github.com>
2025-11-28 23:06:33 +0100

    Initial project

---
+++ src/pypixz/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+""" PyPIxz
+
+PyPIxz module to manage your dependencies in a simple and efficient way while
+maintaining guaranteed security.
+
+Basic usage:
+    import pypixz
+    pypixz.install_module("pypixz", version_range='>=2.0')
+
+    pypixz.install_requirements("requirements.txt")
+
+:copyright: (c) 2025 ZKeepr
+:license: MIT, see LICENSE for more details.
+"""
+
+__all__ = []
